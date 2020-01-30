a8d5acb2e1b4a89894c2570aa570743d25d43fd7
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-01-30 14:14:49 -0800

    add check_md() function that performs the first checks on the passed metadata

--- q2_metadata/_normalize.py
+++ q2_metadata/_normalize.py
@@ -12,6 +12,7 @@ from os.path import abspath, dirname
 import qiime2 as q2
 import pandas as pd
 
+from q2_metadata.normalization._metadata import check_md
 from q2_metadata.normalization._io_utils import get_rules, get_databases
 from q2_metadata.normalization._waving_flags import show_rules_structure_issues
 
@@ -26,8 +27,12 @@ def normalize(metadata: q2.Metadata) -> q2.Metadata:
         A curated metadata table.
     """
 
+
+
     # Get metadata as pandas data frame
     md = metadata.to_dataframe()
+
+    md_valid = check_md(md)
 
     # Get path of the executable
     root_dir = dirname(abspath(__file__))
