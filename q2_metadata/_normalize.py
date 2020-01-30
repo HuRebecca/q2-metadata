9dc6127de10489c980b11016256036af9fa67d59
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-01-29 16:41:13 -0800

    remive the function of this main file to q2_metadata/normalization/_io_utils.py

--- q2_metadata/_normalize.py
+++ q2_metadata/_normalize.py
@@ -6,11 +6,16 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-import yaml
-import pandas as pd
-from glob import glob
-from os.path import abspath, basename, dirname, isfile, splitext
+import sys
+from os.path import abspath, dirname
+
 import qiime2 as q2
+import pandas as pd
+
+from q2_metadata.normalization._io_utils import (
+
+)
+from .normalization._waving_flags import show_rules_structure_issues
 
 
 # annotate how types are expected to be passed to the function
@@ -22,15 +27,23 @@ def normalize(metadata: q2.Metadata) -> q2.Metadata:
     :return:
         A curated metadata table.
     """
-    # Get hard-coded but possible user-modified data from the "databases" and "rules" folders
-    root_dir = dirname(abspath(__file__))
-    databases_paths, rules_paths = get_paths(root_dir)
-    rules = get_rules(rules_paths)
-    databases = get_databases(databases_paths)
 
+    # Get path to metadata dataframe
     md = metadata.to_dataframe()
 
-    # md_out = normalize()
+    # Get hard-coded but possible user-modified data from the "databases" and "rules" folders
+    root_dir = dirname(abspath(__file__))
+
+    rules, rules_dict, issues = get_rules(root_dir, md)
+
+    if issues:
+        show_rules_structure_issues(issues)
+        sys.exit(1)
+
+    databases = get_databases(root_dir)
+
+
+    # md_out = do_normalize(md, rules)
 
     # only during dev so that the function return something :)
     md_out = pd.DataFrame({
@@ -39,110 +52,3 @@ def normalize(metadata: q2.Metadata) -> q2.Metadata:
         'col2': ['3', '4']
     }).set_index('sampleid')
     return q2.Metadata(md_out)
-
-
-def check_file_existence(path: str, data: str) -> bool:
-    if data == 'rule' and path.endswith('.yml'):
-        return True
-    elif data == 'database' and path.endswith('.csv'):
-        return True
-    else:
-        print('Incorrect file extension %s (.yml or .csv)' % path)
-        return False
-
-
-def parse_yml_file(yml_path: str) -> dict:
-    """
-    Parse the non-comment contents of a yaml file.
-    :param yml_path:
-        Path to one metadata column .yml rules
-    :return rule:
-    """
-    if not check_file_existence(yml_path, 'rule'):
-        return {}
-
-    # read the lines
-    lines = []
-    with open(yml_path, 'r') as f:
-        for line in f:
-            lines.append(line.strip())
-
-    # open read the yaml file and skip the comment lines in the handle
-    comment_lines_indices = [x for x, line in enumerate(lines) if line.startswith('#')]
-    with open(yml_path, 'r') as handle:
-        for comment_line in range(comment_lines_indices[-1]):
-            _ = handle.readline()
-        rule = yaml.load(handle, Loader=yaml.FullLoader)
-    return rule
-
-
-def get_databases(db_paths: dict) -> dict:
-    """
-    :param db_paths:
-        Key     : databases name
-        Value   : paths to .csv database
-    :return:
-        Key     : databases name
-        Value   : pd.DataFrame()
-    """
-    databases = {}
-    for db, db_path in db_paths.items():
-        if not check_file_existence(db_path, 'database'):
-            return {}
-        database = pd.read_csv(
-            db_path, header=0, sep=',', dtype=str, low_memory=False
-        )
-        databases[db] = database
-    return databases
-
-
-def get_rules(rules_paths: dict) -> dict:
-    """
-    Collect the
-    :param rules_paths:
-        Keys    : metadata columns
-        Values  : path to the yaml file rules
-    :return:
-        Keys    : metadata columns
-        Values  : rules dicts (from yaml to dict using yaml.load())
-    """
-    rules = {}
-    for col, rule_path in rules_paths.items():
-        rule = parse_yml_file(rule_path)
-        rules[col] = rule
-    return rules
-
-
-def get_paths(root_dir: str) -> (dict, dict):
-    """
-    Get the path to the databases of ontology terms
-    and per-metadata column yaml rules files ad
-    :param root_dir:
-        Directory containing the databases or rules folder
-    :return:
-        First dict is for the databases paths (values) per database (key)
-        Second dict is for the rules paths (values) per metadata column (key)
-    """
-    databases_paths = get_paths_dict(root_dir, "databases")
-    rules_paths = get_paths_dict(root_dir, "rules")
-    return databases_paths, rules_paths
-
-
-def get_paths_dict(root_dir: str, db_rule: str) -> dict:
-    """
-    :param root_dir:
-        Directory containing the databases or rules folder
-    :param db_rule:
-        "databases" if key is the folder
-        "rules" if key is in the file name
-    :return: file paths as values per database/rule as key
-    """
-    if db_rule == 'databases':
-        paths = glob('%s/%s/*/*.csv' % (root_dir, db_rule))
-        cur_dict = dict([path.split('/')[-2], path] for path in paths)
-    elif db_rule == 'rules':
-        paths = glob('%s/%s/*.yml' % (root_dir, db_rule))
-        cur_dict = dict([splitext(basename(path))[0], path] for path in paths)
-    else:
-        cur_dict = {}
-    return cur_dict
