9e063b810fe94b58672177b9bc2b1299aabbf175
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-02-03 16:57:51 -0800

    edit flags reporter function (only for rules format so far) to not print

--- q2_metadata/normalization/_flags.py
+++ q2_metadata/normalization/_flags.py
@@ -18,9 +18,8 @@ def show_rules_structure_issues(root_dir: str, issues: list) -> None:
     out_flags = '%s/normalization/outputs/rules_check.txt' % root_dir
     with open(out_flags, 'w') as o:
         for error_type, error_type_pd in issues_pd.groupby('message'):
-            print('[%s cases] "%s"' % (error_type_pd.shape[0], error_type))
+            # print('[%s cases] "%s"' % (error_type_pd.shape[0], error_type))
             o.write('[%s cases] "%s"\n' % (error_type_pd.shape[0], error_type))
             for error_file, error_file_pd in error_type_pd.groupby('file'):
-                print('%s: %s' % (error_file, ', '.join(error_file_pd.rule.tolist())))
+                # print('%s: %s' % (error_file, ', '.join(error_file_pd.rule.tolist())))
                 o.write('\t%s: %s\n' % (error_file, ', '.join(error_file_pd.rule.tolist())))
-
